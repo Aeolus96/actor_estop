f2fa8f7a480ac28ab494ffdb9a618ed0c3245ed1
Aeolus96 <96811105+Aeolus96@users.noreply.github.com>
2024-05-12 00:26:42 -0400

    debug

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+# Set ROS IP
+echo "Setting ROS IP..."
+ROS_IP=$(hostname -I | cut -d' ' -f1)
+echo "export ROS_IP=$ROS_IP" >> $HOME/.bashrc
+
 # Set ROS MASTER URI
 echo "Setting ROS MASTER URI..."
+read -p "PLEASE ENTER ROS MASTER'S IP (0.0.0.0): " MASTER_IP
+echo "export ROS_MASTER_URI=http://$MASTER_IP:11311/" >> $HOME/.bashrc
 
-echo "export ROS_IP=$(hostname -I | cut -d' ' -f1)" >> $HOME/.bashrc
-read -p "PLEASE ENTER ROS MASTER'S IP (0.0.0.0): " master_ip
-echo "export ROS_MASTER_URI=http://$master_ip:11311/" >> $HOME/.bashrc
-
-# Create a shell script to run your ROS node --------------
+# Create a shell script to run your ROS node
 echo "Creating ROS node launch script..."
 
 mkdir -p $HOME/catkin_ws/src/actor_estop/temp
 echo "#!/bin/bash" > $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
-echo "source /opt/ros/noetic/setup.bash" >> $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
-echo "source ~/catkin_ws/devel/setup.bash" >> $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
-echo "rosrun actor_estop/temp edge_estop_manager.py" >> $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
+echo "source $HOME/.bashrc" >> $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
+echo "rosrun actor_estop edge_estop_manager.py" >> $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
 
 # Make the script executable
 echo "Making script executable..."
 
 sudo chmod +x $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh
 
-# Create a systemd service --------------------------------
+# Create a systemd service
 echo "Creating systemd service..."
 
 echo "[Unit]" > $HOME/catkin_ws/src/actor_estop/temp/estop.service
@@ -30,7 +32,9 @@ echo "After=network.target" >> $HOME/catkin_ws/src/actor_estop/temp/estop.servic
 echo "" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
 echo "[Service]" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
 echo "User=$USER" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
-echo "ExecStart=/bin/bash $HOME/catkin_ws/src/actor_estop/temp/rosrun_estop.sh" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
+echo "Environment=\"ROS_IP=$ROS_IP\"" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
+echo "Environment=\"ROS_MASTER_URI=$MASTER_IP\"" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
+echo "ExecStart=/bin/bash /home/ubuntu/catkin_ws/src/actor_estop/temp/rosrun_estop.sh >> /home/ubuntu/catkin_ws/src/actor_estop/temp/rosrun_estop.log 2>&1" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
 echo "Restart=always" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
 echo "RestartSec=5" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
 echo "" >> $HOME/catkin_ws/src/actor_estop/temp/estop.service
